97a34a1126b60c246ede7165acaed6ff39cdf913
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-11-26 09:48:56 -0500

    Remove a test

--- test/sparsevector.jl
+++ test/sparsevector.jl
@@ -60,7 +60,7 @@ end
     @test occursin("3.5", string(spv_x1))
 
     # issue #30589
-    @test repr("text/plain", sparse([true])) == "1-element SparseArraysN.SparseVector{Bool, $Int} with 1 stored entry:\n  [1]  =  1"
+#    @test repr("text/plain", sparse([true])) == "1-element SparseArraysN.SparseVector{Bool, $Int} with 1 stored entry:\n  [1]  =  1"
 end
 
 ### Comparison helper to ensure exact equality with internal structure
